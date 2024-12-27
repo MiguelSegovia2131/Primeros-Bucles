d452dbcd42a0df6de717f6c4ab63e3e08c84615f
Miguel <miguelsegoviacastillo@hotmail.com>
2024-12-27 14:59:25 -0300

    Primer Commit Bucles

---
+++ bucles.js
@@ -0,0 +1,23 @@
+for (let i = 0; i <= 30; i = i + 2){
+    console.log(i);
+}
+
+for (let i = 100; i >= 0; i -= 4){
+        console.log(i);
+}
+
+for (let i = 10;i >= -5; i -=3){
+    console.log(i);
+}
+
+let suma = 0;
+for (let i = 0; i <= 50; i += 2){
+    suma += i;
+}
+console.log(suma);
+
+let multiplicacion = 1;
+for (let i = 1; i <= 20; i++){
+    multiplicacion *= i;
+}
+console.log(multiplicacion);
